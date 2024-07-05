2e68192f37b3d4a209718ff9c50cf927fd155a69
OmairMedia <omairmedia2020@gmail.com>
2024-07-05 20:01:20 +0500

    latest

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Admin;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Admin::create([
+            'name' => 'Wheeler Admin',
+            'email' => 'admin@wheeler.co.uk',
+            'password' => Hash::make('WheelerAdmin123!@#'),
+        ]);
     }
 }
